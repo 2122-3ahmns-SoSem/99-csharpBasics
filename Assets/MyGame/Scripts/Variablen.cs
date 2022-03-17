83367741becc5f62fb3ae5a44b65cc6ab30f9e14
smeerws <hello@smeerws.at>
2022-03-17 10:23:49 +0100

    tostring

--- Assets/MyGame/Scripts/Variablen.cs
+++ Assets/MyGame/Scripts/Variablen.cs
@@ -10,8 +10,9 @@ public enum Himmelsrichtungen
 
 public class Variablen : MonoBehaviour
 {
+    public GameObject x;
     Color farbe;
-    int anzTueren;
+    int anzTueren = 4;
     bool fahren;
 
     // Start is called before the first frame update
@@ -20,6 +21,21 @@ public class Variablen : MonoBehaviour
         Debug.Log(
             Himmelsrichtungen.Nord + " " + Himmelsrichtungen.Sued + "" +
             Himmelsrichtungen.West + " " + Himmelsrichtungen.Ost);
+
+        //
+        farbe = Color.red;
+        Debug.Log("Red " + farbe.ToString());
+
+        farbe = Color.yellow;
+        Debug.Log("Yellow " +farbe.ToString());
+
+        farbe = Color.white;
+        Debug.Log("White " + farbe.ToString());
+
+        Debug.Log(x.ToString());
+        Debug.Log(anzTueren.ToString());
+        Debug.Log("bool fahren " + fahren.ToString());
+
     }
 
     // Update is called once per frame
